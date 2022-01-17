7dda72faedd31a7103c4df7d5acb0a72314924e1
irfanmaulana126 <irfanmaulana126@gmail.com>
2022-01-17 17:18:15 +0700

    my first token solidty

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-const DappToken = artifacts.require("DappToken");
+var IMTKToken = artifacts.require("./IMTKToken.sol");
+var IMTKTokenSale = artifacts.require("./IMTKTokenSale.sol");
 
-module.exports = function (deployer) {
-  deployer.deploy(DappToken);
+module.exports = function(deployer) {
+  deployer.deploy(IMTKToken, 1000000).then(function() {
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(IMTKTokenSale, IMTKToken.address, tokenPrice);
+  });
 };
